415fc8ac312aafc68a12d067cce9cb8a9a3ba4ee
Wasabiii <77311141+FeblesLuis@users.noreply.github.com>
2025-02-18 12:23:02 -0400

    Delete DSW_ApiNoConformidades-Dollder_MS directory

--- DSW_ApiNoConformidades-Dollder_MS/DSW_ApiNoConformidades-Dollder_MS/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DSW_ApiNoConformidades_Dollder_MS.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
